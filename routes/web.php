7f646189699c436294c0cfcf36debe650953b78c
taehyungggg29 <deliapuspitasari554@gmail.com>
2023-06-22 16:21:52 +0700

    fix

---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/welcome', function () {
+    echo "Selamat datang di Laravel";
+});
+Route::get('/greeting', function () {
+    return view('greeting');
+});
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('/mobil',[MobilController::class, 'index']);
+Route::get('/mobil/create',[MobilController::class, 'create']);
+Route::post('/mobil/simpan-data',[MobilController::class, 'store']);
+
+Route::get('/merk',[MerkController::class,'index']);
+Route::get('/merk/create',[MerkController::class,'create']);
+Route::post('/merk/simpan-data',[MerkController::class,'store']);
+
+Route::get('/merk/edit/{id}',[MerkController::class, 'edit']);
+Route::post('/merk/update/{id}',[MerkController::class, 'update']);
+Route::get('/merk/delete/{id}',[MerkController::class, 'delete']);
+
+//route tipemobil
+Route::get('/tipemobil',[TipeMobilController::class, 'index']);
+Route::get('/tipemobil/create',[TipeMobilController::class, 'create']);
+Route::post('/tipemobil/simpan-data',[TipeMobilController::class, 'store']);
+Route::get('/tipemobil/edit/{id}', [TipeMobilController::class, 'edit']);
+Route::post('/tipemobil/update/{id}',[TipeMobilController::class, 'update']);
+Route::get('/tipemobil/delete/{id}', [TipeMobilController::class, 'delete']);
+});
+Route::get('/login', [Auth\LoginController::class, 'index'])->name('login');
+Route::post('/login/proses', [Auth\LoginController::class, 'login']);
+Route::get('/logout', [Auth\LoginController::class, 'logout']);
+
+Route::get('/register', [Auth\RegisterController::class, 'index']);
+Route::post('/register/proses', [Auth\RegisterController::class, 'register']);
+
+
